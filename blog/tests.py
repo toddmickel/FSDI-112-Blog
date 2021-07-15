f670f5ee0de6bb999677c79a5881ba89a38e0db3
Todd Mickel <todd@toddmickel.com>
2021-07-14 20:09:49 -0600

    Added forms to manipulate blog

--- blog/tests.py
+++ blog/tests.py
@@ -40,3 +40,35 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "A title")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/posts/1/')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New body',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New title')
+        self.assertEqual(Post.objects.last().body, 'New body')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated body',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_create_view_uses_correct_template(self):
+        response = self.client.get("/posts/new/")
+        self.assertTemplateUsed(response, "post_new.html")
+
+    def test_post_update_view_uses_correct_template(self):
+        response = self.client.get("/posts/1/edit/")
+        self.assertTemplateUsed(response, "post_edit.html")
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import (
+    BlogListView, 
+    BlogDetailView, 
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
-    path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("posts/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("posts/new/", BlogCreateView.as_view(), name="post_new"),
+    path("posts/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("posts/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
 ]
